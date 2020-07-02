5d6558db46105556df1263f60b147b451f58a735
Daniel Fernández-González <dannynyc3@gmail.com>
2020-07-02 19:12:56 +0200

    Update run_L2R_parser.sh

--- scripts/run_L2R_parser.sh
+++ scripts/run_L2R_parser.sh
@@ -4,7 +4,6 @@ python scripts/L2RParser.py --mode FastLSTM --num_epochs 1000 --batch_size 32 --
  --pos_dim 100 --char_dim 100 --num_filters 100 --arc_space 512 --type_space 128 \
  --opt adam --learning_rate 0.001 --decay_rate 0.75 --epsilon 1e-4 --coverage 0.0 --gamma 0.0 --clip 5.0 \
  --schedule 20 --double_schedule_decay 5 \
- --grandPar  --sibling \
  --p_in 0.33 --p_out 0.33 --p_rnn 0.33 0.33 --unk_replace 0.5 --label_smooth 1.0 --pos --char --beam 10 --prior_order inside_out \
  --word_embedding sskip --word_path "data/sskip.eng.100.gz" --char_embedding random \
  --punctuation '.' '``' "''" ':' ',' \
